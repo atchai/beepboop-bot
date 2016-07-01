ae1a05acf0bd04fc7e9985c33dd4b2b640cad366
John Griffin <john@atchai.com>
2016-07-01 11:43:44 +0100

    Update to allow multi-team mode

--- index.js
+++ index.js
@@ -8,22 +8,28 @@ var controller = Botkit.slackbot({
   debug: false
 })
 
-// Connect to Slack RTM 
-controller.spawn({
-  token: token
-}).startRTM(function (err, bot, payload) {
-  if (err) {
-    throw new Error(err)
-  }
-  console.log('Connected to Slack RTM')
-})
+// Assume single team mode if we have a SLACK_TOKEN
+if (token) {
+  console.log('Starting in single-team mode')
+  controller.spawn({
+    token: token
+  }).startRTM(function (err, bot, payload) {
+    if (err) {
+      throw new Error(err)
+    }
 
-// Say "I'm here" when we're invited to join a channel
+    console.log('Connected to Slack RTM')
+  })
+// Otherwise assume multi-team mode - setup beep boop resourcer connection
+} else {
+  console.log('Starting in Beep Boop multi-team mode')
+  require('beepboop-botkit').start(controller, { debug: true })
+}
+
 controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
 
-// Say "Hello" when we hear "hi"
 controller.hears(['hi'], ['ambient', 'direct_message','direct_mention','mention'], function (bot, message) {
   bot.reply(message, 'Hello.')
 })
